8f0d8cbfed9fb95c7bcff8af9fa6f58e5d198fe9
niyashameer <sniya359@gmail.com>
2022-02-01 23:54:56 +0530

    🎨 Improve music ui

--- components/work/Music.tsx
+++ components/work/Music.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SvgRenderer from "../../shared/component/svgRenderer";
 
 const Music = () => {
+	useEffect(() => {
+		const player = document.getElementById('audio')
+	})
 	return (
-		<div className='relative bg-darkBlue h-screen p-20'>
-			<div className='w-10/12 mx-auto font-[SF_Pro_Display_Regular] text-white font-bold text-4xl'>
+		<div className='relative bg-darkBlue h-[150vh] p-20'>
+			<div className='relative w-10/12 mx-auto font-[SF_Pro_Display_Regular] text-white font-bold text-4xl'>
 				<div className='w-max mx-auto'>music</div>
 				<SvgRenderer filePath='assets/vectors/music.svg' />
 
-				<audio controls className='w-10/12 mx-auto'>
+				<audio id="audioplayer"className='w-10/12 mx-auto'>
 					<source
 						src='assets/audio/master.mp3'
 						type='audio/mp3'
@@ -16,11 +19,11 @@ const Music = () => {
 					/>
 				</audio>
 			</div>
-			<div className='absolute -bottom-0 -left-20 bg-black w-40 h-40 rounded-full flex items-center justify-center'>
-				<div className='relative bg-gray w-24 h-24 rounded-full self-center'></div>
+			<div className='bg-black w-60 h-60 rounded-full drop-shadow-3xl flex items-center justify-center mx-auto my-20'>
+					<div className='relative bg-[#B6A49C] w-24 h-34 rounded-full self-center drop-shadow-3xl'><SvgRenderer filePath="assets/vectors/play.svg"/></div>
 			</div>
 		</div>
 	);
 };
 
-export default Music
+export default Music;
