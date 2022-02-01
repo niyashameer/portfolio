fdf2163730c10a937228dfc92a5d78519182fad5
niyashameer <sniya359@gmail.com>
2022-02-01 17:34:13 +0530

    💄 Update tailwind config custom drop shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,34 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./shared/**/*.{js,ts,jsx,tsx}",
-    "./assets/**/*.svg",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        lightBlack: "#263238",
-        orange: "#F67833",
-        gray: "#A1A1B0",
-        darkBlue: "#17419E",
-        retro: "#7F7575",
-        darkRed: "#A3112E"
-      },
-      fontFamily: {
-        primary: ["SF-Pro-Display-Light"],
-        primaryBold: ["SF-Pro-Display-Bold"],
-        primarySemiBold: ["SF-Pro-Display-Semibold"],
-        primaryMedium: ["SF-Pro-Display-Medium"],
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	purge: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./shared/**/*.{js,ts,jsx,tsx}",
+		"./assets/**/*.svg",
+	],
+	theme: {
+		extend: {
+			colors: {
+				lightBlack: "#263238",
+				orange: "#F67833",
+				gray: "#A1A1B0",
+				darkBlue: "#17419E",
+				retro: "#7F7575",
+				darkRed: "#A3112E",
+			},
+			fontFamily: {
+				primary: ["SF-Pro-Display-Light"],
+				primaryBold: ["SF-Pro-Display-Bold"],
+				primarySemiBold: ["SF-Pro-Display-Semibold"],
+				primaryMedium: ["SF-Pro-Display-Medium"],
+			},
+			dropShadow: {
+				"3xl": "0 25px 25px rgba(0, 0, 0, 0.45)",
+			},
+		},
+	},
+	plugins: [],
+};
