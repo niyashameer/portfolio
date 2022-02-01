7732eb7775a4715bf96525c5abb894be05638482
niyashameer <sniya359@gmail.com>
2022-02-01 23:54:35 +0530

    ✨ Introduce projects

--- components/work/Projects.tsx
+++ components/work/Projects.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Projects = () => {
-  return <div className='h-screen w-full relative'></div>;
+	return (
+		<div className='h-screen w-full relative px-14 py-14'>
+			<div className='w-max text-lightBlack font-bold text-4xl font-lightBlack'>
+				projects
+            </div>
+            <div className='rounded-full w-48 h-48 bg-darkBlue absolute top-24 -right-24'></div>
+            <div className="grid grid-cols-3 gap-4 w-[70%] mx-auto py-20">
+                <div className="rounded-lg border border-orange w-72 h-72"></div>
+                <div className="rounded-lg border border-orange w-72 h-72"></div>
+                <div className="rounded-lg border border-orange w-72 h-72"></div>
+            </div>
+		</div>
+	);
 };
 
-export default Projects
+export default Projects;
