8d61a5cfce1cdba9b8889a9de9f91fe57f21863a
niyashameer <sniya359@gmail.com>
2022-02-16 18:39:54 +0530

    💄 Add carousel

--- components/work/Projects.tsx
+++ components/work/Projects.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import EmblaCarousel from "../../shared/component/embla/EmblaCarousel";
 
+const SLIDE_COUNT = 5;
+const slides = Array.from(Array(SLIDE_COUNT).keys());
 const Projects = () => {
+	const [emblaRef] = useEmblaCarousel();
 	return (
-		<div className='h-screen w-full relative px-14 py-14'>
-			<div className='w-max text-lightBlack font-bold text-4xl font-lightBlack'>
+		<div className='h-screen w-full relative overflow-hidden' ref={emblaRef}>
+			<div className='w-max text-lightBlack font-bold text-4xl font-lightBlack pt-14 px-14'>
 				projects
-            </div>
-            <div className='rounded-full w-48 h-48 bg-darkBlue absolute top-24 -right-24'></div>
-            <div className="grid grid-cols-3 gap-4 w-[70%] mx-auto py-20">
+			</div>
+			<div className='flex justify-center items-center h-[80%]'>
+				<div className='h-[60%] w-full flex flex-col items-center justify-center place-content-center'>
+					<div className='rounded-full w-56 h-56 bg-darkBlue absolute top-24 -right-24'></div>
+					{/* <div className="grid grid-cols-3 gap-4 w-[70%] mx-auto py-20 items-center">
                 <div className="rounded-lg border border-orange w-72 h-72"></div>
                 <div className="rounded-lg border border-orange w-72 h-72"></div>
                 <div className="rounded-lg border border-orange w-72 h-72"></div>
-            </div>
+            </div> */}
+					<EmblaCarousel slides={slides} />
+				</div>
+			</div>
 		</div>
 	);
 };
